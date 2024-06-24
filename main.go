d0d5db4b18c80b6c97d75cd3bcfe4775e50b3758
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-06-24 10:51:14 +0800

    feat: initial REPL loop

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello there")
+	scanner := bufio.NewScanner(os.Stdin)
+	options := getOptions()
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+		fmt.Println("you entered: ", input)
+
+		option := options[input]
+		option.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getOptions() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to Pokedex! \nUsage: \n")
+
+	options := getOptions()
+
+	for _, opt := range options {
+		fmt.Printf("%v: %v\n", opt.name, opt.description)
+	}
+
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exit the program")
+	os.Exit(0)
+	return nil
 }
